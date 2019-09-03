b1863ccd398942d3364086db2ac241e70d714886
gilKag2 <35850376+gilKag2@users.noreply.github.com>
2019-09-03 17:20:24 +0300

    Done

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,29 +2,89 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
+    private Button topButton;
+    private Button bottomButton;
 
+    private TextView story;
+
+    private int index = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        topButton = findViewById(R.id.buttonTop);
+        bottomButton = findViewById(R.id.buttonBottom);
+        story = findViewById(R.id.storyTextView);
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        topButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (index < 3) {
+                    moveToThirdStory();
+                } else {
+                    T6End();
+                }
+            }
+        });
 
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
+        bottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (index == 1) {
+                    moveToSecondStory();
+                }
+                else if (index == 2) {
+                    T4End();
+                } else if (index == 3) {
+                    T5End();
+                }
+            }
+        });
 
     }
+
+    private void hideButtons() {
+        topButton.setVisibility(View.GONE);
+        bottomButton.setVisibility(View.GONE);
+    }
+
+    private void T4End() {
+        story.setText(R.string.T4_End);
+        hideButtons();
+    }
+
+    private void T5End() {
+        story.setText(R.string.T5_End);
+        hideButtons();
+    }
+
+    private void T6End() {
+        story.setText(R.string.T6_End);
+        hideButtons();
+    }
+
+    private void moveToSecondStory() {
+        story.setText(R.string.T2_Story);
+        topButton.setText(R.string.T2_Ans1);
+        bottomButton.setText(R.string.T2_Ans2);
+        index = 2;
+    }
+
+    private void moveToThirdStory() {
+        story.setText(R.string.T3_Story);
+        topButton.setText(R.string.T3_Ans1);
+        bottomButton.setText(R.string.T3_Ans2);
+        index = 3;
+    }
+
+
 }
